b605a5f3c93885058ab67aecbe132bed02347a5c
Sergio Quevedo <sergio.quevedo1@educa.madrid.org>
2024-09-18 10:50:20 +0200

    ejercicio1

---
+++ Ejercicios/String/Ejercicio1.php
@@ -0,0 +1,12 @@
+<HTML>
+<H1> Ejercicio 1 </H1>
+<BODY>
+
+<?php
+   $ip="192.18.16.204";
+printf("IP " . $ip . "en binario es %b",$ip);
+$ip2="10.33.161.2";
+printf("IP " . $ip2 . "en binario es %b",$ip2);
+?>
+</BODY>
+</HTML>
