371c9de36202da85f328bf65e47f9a2c7a20d8cf
Sergio Quevedo <sergioquero@gmail.com>
2024-09-19 12:47:24 +0200

    ejercicio1

--- Ejercicios/String/Ejercicio1.php
+++ Ejercicios/String/Ejercicio1.php
@@ -4,14 +4,12 @@
 
 <?php
     $ip="192.18.16.204";
-    $dirIP;
-    $dirIP = explode('.',$IP);
+    $dirIP = explode('.',$ip);
     printf("<p>IP " . $ip . " en binario es %b",$dirIP[0],$dirIP[1],$dirIP[2],$dirIP[3] . "</p>");
 
 
     $ip2="10.33.161.2";
-    $dirIP2;
-    $dirIP2 = explode('.',$IP2);
+    $dirIP2 = explode('.',$ip2);
     printf("<p>IP " . $ip2 . "en binario es %b ",$dirIP2[0],$dirIP2[1],$dirIP2[2],$dirIP2[3] . "</p>");
 ?>
 </BODY>
