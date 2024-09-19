c0acc67d0ae76f99d525ecf9ac2fd7e8f3fd41cb
Sergio Quevedo <sergioquero@gmail.com>
2024-09-19 13:03:06 +0200

    ejercicio2

---
+++ Ejercicios/String/Ejercicio2.php
@@ -0,0 +1,15 @@
+<HTML>
+<H1> Ejercicio 1 </H1>
+<BODY>
+
+<?php
+    $ip="192.18.16.204";
+    $dirIP = explode(".",$ip);
+    echo "<p>IP " . $ip . " en binario es ". decbin($dirIP[0]) .".".decbin($dirIP[1]).".".decbin($dirIP[2]).".".decbin($dirIP[3])." </p>";
+
+    $ip2="10.33.161.2";
+    $dirIP2 = explode(".",$ip2);
+    echo "<p>IP " . $ip2 . " en binario es ". decbin($dirIP2[0]) .".".decbin($dirIP2[1]).".".decbin($dirIP2[2]).".".decbin($dirIP2[3])." </p>";
+?>
+</BODY>
+</HTML>
