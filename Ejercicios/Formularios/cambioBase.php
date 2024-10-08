41c6dee506e7273ba3ee9800b663549a8fd48a32
Sergio Quevedo <sergioquero@gmail.com>
2024-10-08 09:37:25 +0200

    form

---
+++ Ejercicios/Formularios/cambioBase.php
@@ -0,0 +1,28 @@
+<?php
+    $num = limpiar($_REQUEST['num']);
+    $operacion = $_REQUEST['operacion'];
+
+    switch ($operacion) {
+        case 'binario':
+            echo "El ".$num1." en binario es ". decbin($num);
+            break;
+        case 'octal':
+            echo "El ".$num1." en octal es ". decoct($num);
+            break;
+        case 'hexadecimal':
+            echo "El ".$num1." en hexadecimal es ". dechex($num);
+            break;
+        case 'todos':
+            echo "El ".$num1." en binario es ". decbin($num);
+            echo "El ".$num1." en octal es ". decoct($num);
+            echo "El ".$num1." en hexadecimal es ". dechex($num);
+            break;
+    }
+
+    function limpiar($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+?>
