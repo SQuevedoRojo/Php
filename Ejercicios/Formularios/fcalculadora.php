c68c10a7a94cccf3ed2c86ec8fbde23395d4a611
Sergio Quevedo <sergioquero@gmail.com>
2024-10-09 09:19:37 +0200

    form

--- Ejercicios/Formularios/fcalculadora.php
+++ Ejercicios/Formularios/fcalculadora.php
@@ -30,20 +30,25 @@
         switch ($operacion) {
             case '+':
                 $resultado = $num1 + $num2;
-                echo "El resultado de " . $num1 . " " . $operacion . " " . $num2 . " = " . $resultado;
+                imprimir($num1,$num2,$operacion,$resultado);
                 break;
             case '-':
                 $resultado = $num1 - $num2;
-                echo "El resultado de " . $num1 . " " . $operacion . " " . $num2 . " = " . $resultado;
+                imprimir($num1,$num2,$operacion,$resultado);
                 break;
             case '*':
                 $resultado = $num1 * $num2;
-                echo "El resultado de " . $num1 . " " . $operacion . " " . $num2 . " = " . $resultado;
+                imprimir($num1,$num2,$operacion,$resultado);
                 break;
             case '/':
                 $resultado = $num1 / $num2;
-                echo "El resultado de " . $num1 . " " . $operacion . " " . $num2 . " = " . $resultado;
+                imprimir($num1,$num2,$operacion,$resultado);
                 break;
+        }
+    
+        function imprimir($num1,$num2,$operacion,$resultado)
+        {
+            echo "El resultado de ".$num1." ".$operacion." ".$num2." = ".$resultado;
         }
     }
 ?>
