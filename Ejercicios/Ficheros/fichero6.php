c7ba20187d0ec5aff8362a3238ea390df59be63a
Sergio Quevedo <sergioquero@gmail.com>
2024-10-15 08:43:49 +0200

    ficheros

---
+++ Ejercicios/Ficheros/fichero6.php
@@ -0,0 +1,53 @@
+<HTML>
+<H1>Ejercicio 5</H1>
+<BODY>
+<h1>Operaciones Ficheros</h1>
+    <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
+        Fichero (path/nombre) :<input type="text" name="fichero" required><br>
+        <input type="submit" value="enviar">
+        <input type="reset" value="borrar">
+    </form>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+            function recogerDatos()
+            {
+                if(isset($_REQUEST['fichero']))
+                {
+                    $archivo = $_REQUEST['fichero'];
+                    saberDatosFichero($archivo);
+                }
+                else
+                    print("Debe seleccionar una opcion");
+            }
+
+            function saberDatosFichero($archivo)
+            {
+                $rutaArchivo = comprobarRuta($archivo);
+                $file = fopen($rutaArchivo,"r") or die ("No se encuentra el archivo");
+                print "<h3>Nombre del Fichero</h3> ". basename($rutaArchivo);
+                print "<h3>Directorio</h3> " . dirname($rutaArchivo);
+                print "<h3>Tamaño del fichero</h3> " . filesize($rutaArchivo);
+                print "<h3>Ultima modificación</h3> " . filectime($rutaArchivo);
+                fclose($file);
+            }
+
+
+            function comprobarRuta($archivo)
+            {
+                $rutaFichero = "";
+                if(strtolower($archivo[0]) == "c" || $archivo[0] == "/")
+                {
+                    $rutaFichero = realpath($archivo);
+                }
+                else
+                    $rutaFichero = "C:\\wamp64\\www\\files\\".$archivo;
+                return $rutaFichero;
+            }
+
+            recogerDatos();
+        }
+    ?>
+
+</BODY>
+</HTML>
