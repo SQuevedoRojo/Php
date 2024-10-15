09a1b368186c2d6f4cfe0b3fb101b614f77c7372
Sergio Quevedo <sergioquero@gmail.com>
2024-10-15 10:46:59 +0200

    fichero

--- Ejercicios/Ficheros/fichero4.php
+++ Ejercicios/Ficheros/fichero4.php
@@ -23,6 +23,24 @@
                 return $data;
             }
 
+            function fichero($linea)
+            {
+                $data = array();
+                if($linea != ""){
+                    $indice = 0;
+                    $data[0] = limpiar(substr($linea,0,strpos($linea,"##")));
+                    $indice = strpos($linea,"##",$indice)+2;
+                    $data[1] = limpiar(substr($linea,$indice,strpos($linea,"##",$indice)-$indice));
+                    $indice = strpos($linea,"##",$indice)+2;
+                    $data[2] = limpiar(substr($linea,$indice,strpos($linea,"##",$indice)-$indice));
+                    $indice = strpos($linea,"##",$indice)+2;
+                    $data[3] = limpiar(substr($linea,$indice,strpos($linea,"##",$indice)-$indice));
+                    $indice = strpos($linea,"##",$indice)+2;
+                    $data[4] = limpiar(substr($linea,$indice,strpos($linea,"##",$indice)-$indice));
+                }
+                return $data;
+            }
+
             function imprimir($datos)
             {
                 print "<tr><th>" . $datos[0] ."</th><th>" . $datos[1] ."</th><th>" . $datos[2] ."</th><th>" . $datos[3] ."</th><th>" . $datos[4] ."</th></tr>";
