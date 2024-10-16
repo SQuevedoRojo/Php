024128a730c9b562a0cb6f2fc879de23c186f08f
Sergio Quevedo <sergioquero@gmail.com>
2024-10-16 10:23:11 +0200

    fichero

--- Ejercicios/Ficheros/bolsa6.php
+++ Ejercicios/Ficheros/bolsa6.php
@@ -13,9 +13,9 @@
             $maximaCot = ["",0];
             $minimaCot = ["",9999999999];
             $maximoVol = ["","0"];
-            $minimoVol = ["","9.999.999.999"];
+            $minimoVol = ["","999.999.999"];
             $maximaCap = ["","0"];
-            $minimaCap = ["","9.999.999.999"];
+            $minimaCap = ["","999.999.999"];
             while(!feof($file))
             {
                 $datos = fgets($file);
