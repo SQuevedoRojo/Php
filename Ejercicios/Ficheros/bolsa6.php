188f09d67dde0bdd21023d3527f733c0caa719aa
Sergio Quevedo <sergioquero@gmail.com>
2024-10-16 10:58:53 +0200

    ficheros

--- Ejercicios/Ficheros/bolsa6.php
+++ Ejercicios/Ficheros/bolsa6.php
@@ -3,38 +3,11 @@
 <BODY>
 <h1>Bolsa EJ 6</h1>
 <?php
-    
-
-    
-
-    function separarCampos($linea)
-    {
-        $datos = array();
-        $datos[0] = limpiar(substr($linea,0,23));
-        $datos[1] = limpiar(substr($linea,23,9));
-        $datos[2] = limpiar(substr($linea,32,8));
-        $datos[3] = limpiar(substr($linea,40,8));
-        $datos[4] = limpiar(substr($linea,48,12));
-        $datos[5] = limpiar(substr($linea,60,9));
-        $datos[6] = limpiar(substr($linea,69,9));
-        $datos[7] = limpiar(substr($linea,78,13));
-        $datos[8] = limpiar(substr($linea,91,9));
-        $datos[9] = limpiar(substr($linea,100,5));
-        return $datos;
-    }
-
-    function limpiar($data)
-    {
-        $data = trim($data);
-        return $data;
-    }
-
-    
-
+    include 'funciones_bolsa.php';
 ?>
 <?php
 
-    leerFichero();
+    leerFichero6();
 ?>
 </BODY>
 </HTML>
