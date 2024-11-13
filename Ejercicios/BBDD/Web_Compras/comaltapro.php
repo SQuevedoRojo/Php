217fb9bebbb32810f2201197b30508424085f668
Sergio Quevedo <sergioquero@gmail.com>
2024-11-13 09:25:58 +0100

    BBDD

---
+++ Ejercicios/BBDD/Web_Compras/comaltapro.php
@@ -0,0 +1,22 @@
+<HTML>
+    <?php include_once "funciones_comaltapro.php" ?>
+    <H1>Ejercicio 1 Web Empleados</H1>
+    <BODY>
+        <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
+            Nombre del Producto : <input type="text" name="nombreProducto" required><br>
+            Precio del Producto : <input type="number" name="precioProducto" required><br>
+            <select name="categoriaProducto">
+                <?php imprimirCategorias() ?>
+            </select>
+            <input type="submit" value="enviar">
+            <input type="reset" value="borrar">
+        </form>
+        <?php 
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                list($nombreProducto,$precioProducto,$categoriaProducto) = recogerDatos();
+                insertarProducto($nombreProducto,$precioProducto,$categoriaProducto);
+            }
+        ?>
+</BODY>
+</HTML>
---
+++ Ejercicios/BBDD/Web_Compras/funciones_comaltapro.php
@@ -0,0 +1,83 @@
+<?php
+    include_once "funciones_comunes.php";
+
+    function recogerDatos()
+    {
+        $nombreProducto = limpiar($_POST['nombreProducto']);
+        $precioProducto = ($_POST['precioProducto']);
+        $categoriaProducto = limpiar($_POST['categoriaProducto']);
+        if($precioProducto <= 0)
+            trigger_error("El producto no puede tener un precio de 0 o menor");
+        if(strlen($nombreProducto) < 1)
+            trigger_error("El nombre del producto no puede estar vaio");
+        return [$nombreProducto,$precioProducto,$categoriaProducto];
+    }
+
+    function imprimirCategorias()
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT ID_CATEGORIA as id,NOMBRE from categoria");
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            foreach ($resultado as $row)
+            {
+                print "<option value=".$row["id"].">".$row["NOMBRE"]."</option>";
+            }
+        }
+        catch(PDOException $e)
+        {
+            $conn -> rollBack();
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    function insertarProducto($nombreProducto,$precioProducto,$categoriaProducto)
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT max(ID_PRODUCTO) as id from producto");
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            $codigoNuevo = null;
+            if($resultado[0]["id"] == null)
+            {
+                $codigoNuevo = "P0001";
+            }
+            else
+            {
+                $codigoDevuelto = $resultado[0]["id"];
+                $numeroCodigo = intval(substr($codigoDevuelto,1)) + 1;
+                if($numeroCodigo < 10)
+                    $codigoNuevo =  substr($codigoDevuelto,0,1) . "0" . "0" . "0" . strval($numeroCodigo);
+                elseif($numeroCodigo < 100)
+                    $codigoNuevo =  substr($codigoDevuelto,0,1) . "0". "0". strval($numeroCodigo);
+                elseif($numeroCodigo < 1000)
+                    $codigoNuevo =  substr($codigoDevuelto,0,1) . "0" . strval($numeroCodigo);
+                else
+                    $codigoNuevo =  substr($codigoDevuelto,0,1) . strval($numeroCodigo);
+            }
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->beginTransaction();
+            $stmt = $conn->prepare("INSERT INTO producto (ID_PRODUCTO,NOMBRE,PRECIO,ID_CATEGORIA) VALUES (:idProducto,:nombre,:precio,:idCategoria)");
+            $stmt->bindParam(':idProducto', $codigoNuevo);
+            $stmt->bindParam(':nombre', $nombreProducto);
+            $stmt->bindParam(':precio', $precioProducto);
+            $stmt->bindParam(':idCategoria', $categoriaProducto);
+            $stmt->execute();
+            $conn -> commit();
+            print "<h2>Producto insertado exitosamente</h2>";
+        }
+        catch(PDOException $e)
+        {
+            $conn -> rollBack();
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+?>
