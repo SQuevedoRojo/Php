3a8d888b486488190ce992b7bd72d88a8110d716
Sergio Quevedo <sergioquero@gmail.com>
2024-11-13 14:03:29 +0100

    BBDD

---
+++ Ejercicios/BBDD/Web_Compras/comconstock.php
@@ -0,0 +1,18 @@
+<HTML>
+    <?php include_once "funciones_comconstock.php" ?>
+    <H1>Ejercicio 5 Web Empleados</H1>
+    <BODY>
+        <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
+            <select name="productos"><?php imprimirProductos() ?></select><br>
+            <input type="submit" value="enviar">
+            <input type="reset" value="borrar">
+        </form>
+        <?php 
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                $producto = recogerDatos();
+                MostrarProductosEnAlmacen($cantidadProductos,$producto,$almacen);
+            }
+        ?>
+</BODY>
+</HTML>
---
+++ Ejercicios/BBDD/Web_Compras/funciones_comconstock.php
@@ -0,0 +1,60 @@
+<?php
+    include_once "funciones_comunes.php";
+
+    function recogerDatos()
+    {
+        $producto = $_POST['productos'];
+        return $producto;
+    }
+
+    function imprimirProductos()
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT ID_PRODUCTO,NOMBRE FROM producto");
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            foreach ($resultado as $row)
+            {
+                print "<option value=".$row["ID_PRODUCTO"].">".$row["NOMBRE"]."</option>";
+            }
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    function MostrarProductosEnAlmacen($producto)
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("select LOCALIDAD, p.NOMBRE as NOMRBE,CANTIDAD from almacena a,almacen al,producto p where a.NUM_ALMACEN = al.NUM_ALMACEN AND a.ID_PRODUCTO = p.ID_PRODUCTO where p.ID_PRODUCTO = :idProducto order by LOCALIDAD");
+            $stmt->bindParam(':idProducto', $producto);
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            if($resultado != null)
+            {
+                print "<table border='1'";
+                foreach ($resultado as $row) {
+                    print "<tr><th>".$row["LOCALIDAD"]."</th><th>".$row["NOMRBE"]."</th><th>".$row["CANTIDAD"]."</th></tr>";
+                }
+                print "</table>";
+            }
+            else
+                print "<h2>No hay ningun producto en los almacenes</h2>";
+
+        }
+        catch(PDOException $e)
+        {
+            $conn -> rollBack();
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+?>
