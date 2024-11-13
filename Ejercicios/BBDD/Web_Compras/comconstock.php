f617c357be0ef9d89f7d820e6e80cd91cb1f76dd
Sergio Quevedo <sergioquero@gmail.com>
2024-11-13 14:04:03 +0100

    BBDD

--- Ejercicios/BBDD/Web_Compras/comconstock.php
+++ Ejercicios/BBDD/Web_Compras/comconstock.php
@@ -11,7 +11,7 @@
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
                 $producto = recogerDatos();
-                MostrarProductosEnAlmacen($cantidadProductos,$producto,$almacen);
+                MostrarProductosEnAlmacen($producto);
             }
         ?>
 </BODY>
