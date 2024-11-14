4355ab77eee8c1790485e1f1dd832a7ee7a50e56
Sergio Quevedo <sergioquero@gmail.com>
2024-11-14 13:45:19 +0100

    BBDD

--- Ejercicios/BBDD/Web_Compras/funciones_compro.php
+++ Ejercicios/BBDD/Web_Compras/funciones_compro.php
@@ -67,19 +67,64 @@
             else
             {
                 $numAlamcen = $resultado[0]["almacen"];
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $conn->beginTransaction();
-                $stmt = $conn->prepare("INSERT INTO compra (NIF,ID_PRODUCTO,FECHA_COMPRA,UNIDADES) VALUES (:nif,:idProducto,curdate(),1)");
+                $stmt = $conn->prepare("SELECT NIF,ID_PRODUCTO,FECHA_COMPRA from compra where ID_PRODUCTO = :idProducto AND NIF=:nif");
+                $stmt->bindParam(':idProducto', $producto);
                 $stmt->bindParam(':nif', $cliente);
-                $stmt->bindParam(':idProducto', $producto);
                 $stmt->execute();
-                $stmt = $conn->prepare("UPDATE almacena set cantidad=cantidad-1 where NUM_ALMACEN=:idAlmacen AND ID_PRODUCTO=:idProducto");
-                $stmt->bindParam(':idAlmacen', $numAlamcen);
-                $stmt->bindParam(':idProducto', $producto);
-                $stmt->execute();
-                $conn -> commit();
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $resultado2=$stmt->fetchAll();
 
-                print "<h2>Compra Realizada</h2>";
+                if($resultado2 != null)
+                {
+                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $conn->beginTransaction();
+                    $stmt = $conn->prepare("INSERT INTO compra (NIF,ID_PRODUCTO,FECHA_COMPRA,UNIDADES) VALUES (:nif,:idProducto,curdate(),1)");
+                    $stmt->bindParam(':nif', $cliente);
+                    $stmt->bindParam(':idProducto', $producto);
+                    $stmt->execute();
+                    $stmt = $conn->prepare("UPDATE almacena set cantidad=cantidad-1 where NUM_ALMACEN=:idAlmacen AND ID_PRODUCTO=:idProducto");
+                    $stmt->bindParam(':idAlmacen', $numAlamcen);
+                    $stmt->bindParam(':idProducto', $producto);
+                    $stmt->execute();
+                    $conn -> commit();
+
+                    print "<h2>Compra Realizada</h2>";
+                }
+                else
+                {
+                    if($resultado[0]["FECHA_COMPRA"] = date("Y-m-d"))
+                    {
+                        $fechaHoy = date("Y-m-d");
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $conn->beginTransaction();
+                        $stmt = $conn->prepare("UPDATE compra set UNIDADES = UNIDADES + 1 WHERE NIF = :nif AND IP_PRODUCTO = :idProducto AND FECHA_COMPRA = :fechaHoy");
+                        $stmt->bindParam(':nif', $cliente);
+                        $stmt->bindParam(':idProducto', $producto);
+                        $stmt->bindParam(':fechaHoy', $fechaHoy);
+                        $stmt->execute();
+                        $stmt = $conn->prepare("UPDATE almacena set cantidad=cantidad-1 where NUM_ALMACEN=:idAlmacen AND ID_PRODUCTO=:idProducto");
+                        $stmt->bindParam(':idAlmacen', $numAlamcen);
+                        $stmt->bindParam(':idProducto', $producto);
+                        $stmt->execute();
+                        $conn -> commit();
+                    }
+                    else
+                    {
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $conn->beginTransaction();
+                        $stmt = $conn->prepare("INSERT INTO compra (NIF,ID_PRODUCTO,FECHA_COMPRA,UNIDADES) VALUES (:nif,:idProducto,curdate(),1)");
+                        $stmt->bindParam(':nif', $cliente);
+                        $stmt->bindParam(':idProducto', $producto);
+                        $stmt->execute();
+                        $stmt = $conn->prepare("UPDATE almacena set cantidad=cantidad-1 where NUM_ALMACEN=:idAlmacen AND ID_PRODUCTO=:idProducto");
+                        $stmt->bindParam(':idAlmacen', $numAlamcen);
+                        $stmt->bindParam(':idProducto', $producto);
+                        $stmt->execute();
+                        $conn -> commit();
+
+                        print "<h2>Compra Realizada</h2>";
+                    }
+                }
             }
 
         }
