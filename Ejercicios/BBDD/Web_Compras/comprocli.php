1bdc6718ab2097ab77878eb81c34f6d4088abe78
Sergio Quevedo <sergioquero@gmail.com>
2024-11-25 09:37:00 +0100

    BBDD

--- Ejercicios/BBDD/Web_Compras/comprocli.php
+++ Ejercicios/BBDD/Web_Compras/comprocli.php
@@ -1,18 +1,31 @@
 <HTML>
     <?php include_once "funciones_comprocli.php" ?>
     <H1>Ejercicio 12 Web Empleados</H1>
+    <style>
+        #carrito
+        {
+            float: right;
+        }
+    </style>
     <BODY>
         <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
             Producto a comprar : <select name="productos"><?php imprimirProductos() ?></select>
             Unidades : <input type="text" name="unidades"><br>
-            <input type="submit" value="enviar">
+            <input type="submit" value="Añadir Producto" name="annadirProducto">
             <input type="reset" value="borrar">
+            <br>
+            <input type="submit" value="Comprar Cesta Compra" name="comprarProductos">
+            
         </form>
         <?php 
             if($_SERVER["REQUEST_METHOD"] == "POST")
             {
-                list($productos,$unidades) = recogerDatos();
-                annadirCestaCompra($productos,$unidades);
+                imprimirCestaCompra();
+                if(isset($_POST['annadirProducto']))
+                {
+                    list($productos,$unidades) = recogerDatos();
+                    annadirCestaCompra($productos,$unidades);
+                }
             }
         ?>
 </BODY>
--- Ejercicios/BBDD/Web_Compras/funciones_comprocli.php
+++ Ejercicios/BBDD/Web_Compras/funciones_comprocli.php
@@ -36,4 +36,34 @@
     {
         cookieCestaCompra($producto,$unidad);
     }
+
+    function imprimirCestaCompra()
+    {
+        if(isset($_COOKIE["cestaCompra"]))
+        {
+            $carritoCompra = $_COOKIE["cestaCompra"];
+            $productos = explode("|",$carritoCompra);
+            $conn = conexionBBDD();
+            print "<div id='carrito'><h2>Carrito de la compra</h2>";
+            print "<table border='1'><tr><th>Producto</th><th>Unidades</th></tr>";
+            for ($i=0; $i < count($productos) - 1; $i++)
+            { 
+                $producto = explode(";",$productos[$i]);
+                try
+                {
+                    $stmt = $conn->prepare("SELECT NOMBRE FROM producto where ID_PRODUCTO = :id");
+                    $stmt->bindParam(':id', $producto[0]);
+                    $stmt -> execute();
+                    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                    $resultado=$stmt->fetchAll();
+                }
+                catch(PDOException $e)
+                {
+                    echo "Error: " . $e->getMessage();
+                }
+                print "<tr><td>".$resultado[0]["NOMBRE"]."</td><td>".$producto[1]."</td></tr>";
+            }
+            print "</table></div>";
+        }
+    }
 ?>
