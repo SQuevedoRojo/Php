1cd03bacc289ee7fcc08f8d4b5907ad9ab725995
Sergio Quevedo <sergioquero@gmail.com>
2024-12-16 08:59:24 +0100

    BBDDPedidos

---
+++ Ejercicios/BBDD/Web_Pedidos/funciones_consped.php
@@ -0,0 +1,62 @@
+<?php
+
+    include_once "funciones_comunes.php";
+
+    function imprimirClientes()
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT customerNumber,customerName from customers LIMIT 20");
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            foreach ($resultado as $row)
+                print "<option value=".$row["customerNumber"].">".$row["customerNumber"]."|".$row["customerName"]."</option>";
+        }
+        catch(PDOException $e)
+        {
+            $conn -> rollBack();
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    function recogerDatos()
+    {
+        $cliente = $_POST["clientes"];
+        return $cliente;
+    }
+
+    function mostrarInformacionCliente($cliente)
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT orderNumber,orderDate,status from orders where customerNumber = :cliente");
+            $stmt->bindParam(':cliente', $cliente);
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            print "<table border='1'><tr><th>Numero Pedido</th><th>Fecha Pedido</th><th>Estado Pedido</th><th>Numero Linea</th><th>Nombre Producto</th><th>Cantidad Pedida</th><th>Precio Unidad</th></tr>";
+            foreach ($resultado as $row) {
+                $stmt = $conn->prepare("SELECT orderLineNumber,quantityOrdered,priceEach,productName from ordersdetails o,products p where o.orderNumber = :order and productName = productCode");
+                $stmt->bindParam(':order', $row["orderNumber"]);
+                $stmt -> execute();
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $resultado2=$stmt->fetchAll();
+                foreach ($resultado2 as $row2)
+                {
+                    print "<tr><td>".$row["orderNumber"]."</td><td>".$row["orderDate"]."</td><td".$row["status"]."></td><td>".$row2["orderLineNumber"]."</td><td>".$row2["quantityOrdered"]."</td><td>".$row2["priceEach"]."</td><td>".$row2["productName"]."</td></tr>";
+                }
+            }
+            print "</table>";
+        }
+        catch(PDOException $e)
+        {
+            $conn -> rollBack();
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+?>
---
+++ Ejercicios/BBDD/Web_Pedidos/pe_consped.php
@@ -0,0 +1,19 @@
+<?php 
+    include_once "funciones_consped.php";
+?>
+<html>
+    <head><title>Alta de Pedidos</title></head>
+    <body>
+        <h2>Alta de Pedidos</h2>
+        <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
+            <select name="clientes"><?php imprimirClientes() ?></select>
+        </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            $cliente = recogerDatos();
+            mostrarInformacionCliente($cliente);
+        }
+    ?>
+    </body>
+</html>
