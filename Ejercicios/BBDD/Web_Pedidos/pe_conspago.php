23f33a350581c2f43e0c449f9484551caa146011
Sergio Quevedo <sergioquero@gmail.com>
2024-12-16 10:18:45 +0100

    BBDDPedidos

---
+++ Ejercicios/BBDD/Web_Pedidos/pe_conspago.php
@@ -0,0 +1,36 @@
+<?php 
+    include_once "funciones_conspago.php";
+    include_once "funciones_session.php";
+    iniciarSession();
+    if(!verificarSessionExistente())
+    {
+        eliminarSession();
+    }
+?>
+<html>
+    <head><title>Alta de Pedidos</title></head>
+    <body>
+        <h2>Alta de Pedidos</h2>
+        <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
+            Fecha Inicio : <input type="text" name="fecha1" placeholder="año-mes-dia"><br>
+            Fecha Final : <input type="text" name="fecha2" placeholder="año-mes-dia"><br>
+            <input type="submit" value="Mostrar Informacion" name="mostrarInfo"><br>
+            <input type="submit" value="Cerrar Sesion" name="cerrarSesion"><br>
+        </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            if(isset($_POST["mostrarInfo"]))
+            {
+                list($fecha1,$fecha2) = recogerDatos();
+                mostrarInformacionPago($fecha1,$fecha2);
+            }
+            if(isset($_POST["cerrarSesion"]))
+            {
+                if(verificarSessionExistente())
+                    eliminarSession();
+            }
+        }
+    ?>
+    </body>
+</html>
