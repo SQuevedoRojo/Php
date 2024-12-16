7a3990d91f3375ef0fee591d41291a15c79164b3
Sergio Quevedo <sergioquero@gmail.com>
2024-12-16 10:07:18 +0100

    BBDDPedidos

---
+++ Ejercicios/BBDD/Web_Pedidos/funciones_topprod.php
@@ -0,0 +1,86 @@
+<?php
+
+    include_once "funciones_comunes.php";
+
+    function imprimirTipoProductos()
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT productLine from productlines");
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            foreach ($resultado as $row)
+                print "<option value=\"".$row["productLine"]."\"\>".$row["productLine"]."</option>";
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    function recogerDatos()
+    {
+        $fecha1 = limpiar($_POST["fecha1"]);
+        $fecha2 = limpiar($_POST["fecha2"]);
+        if(!(verificarFecha($fecha1)) || !verificarFecha($fecha2))
+            trigger_error("La fecha no se ha introducido correctamente, el formato es el siguiente : YYYY-MM-DD",E_USER_WARNING);
+        return [$fecha1,$fecha2];
+    }
+
+    function verificarFecha($fecha)
+    {
+        $correcto = true;
+        $regex = '/^((((\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))|((\d{4})-(0[13-9]|1[0-2])-(0[1-9]|[12]\d|30))|((\d{4})-02-(0[1-9]|1\d|2[0-8])))|((\d{4})-02-29))$/';
+        if(!preg_match($regex,$fecha))
+            $correcto = false;
+        return $correcto;
+    }
+
+    function mostrarInformacionProductosVendidos($fecha1,$fecha2)
+    {
+        
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT orderNumber from orders where orderDate >= :fecha1 and orderDate <= :fecha2");
+            $stmt->bindParam(':fecha1', $fecha1);
+            $stmt->bindParam(':fecha2', $fecha2);
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+            foreach ($resultado as $row)
+            {
+                $productos = array();
+                $stmt = $conn->prepare("SELECT productName,quantityOrdered from orderdetails o,products p where orderNumber = :numeroPed and o.productCode = p.productCode");
+                $stmt->bindParam(':numeroPed', $row["orderNumber"]);
+                $stmt -> execute();
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $resultado2=$stmt->fetchAll();
+                foreach ($resultado2 as $row2)
+                {
+                    if(in_array($row2["productName"],$productos))
+                    {
+                        $productos[$row2["productName"]] += intval($row2["quantityOrdered"]);
+                    }
+                    else
+                    {
+                        $productos[$row2["productName"]] = intval($row2["quantityOrdered"]);
+                    }
+                }
+            }
+            print "<table border='1'><tr><th>Producto</th><th>Cantidades Vendidas</th></tr>";
+            foreach ($productos as $nombre => $cantidad) {
+                print "<tr><td>$nombre</td><td>$cantidad</td></tr>";
+            }
+            print "</table>";
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+    }
+?>
--- Ejercicios/BBDD/Web_Pedidos/pe_inicio.php
+++ Ejercicios/BBDD/Web_Pedidos/pe_inicio.php
@@ -12,7 +12,11 @@
         <h2>Opciones para el cliente</h2>
         <ul>
             <li><a href="./pe_altaped.php">Compra de Productos</a></li>
-            <li><a href="./comconscli.php">Ver Compras Anteriores</a></li>
+            <li><a href="./pe_consped.php">Consultar Informacion Clientes</a></li>
+            <li><a href="./pe_consprodstock.php">Consultar Stock Producto</a></li>
+            <li><a href="./pe_constock.php">Consultar Stock de Tipo de Producto</a></li>
+            <li><a href="./pe_topprod.php">Mostrar Productos Vendidos Entre Dos Fechas</a></li>
+            <li><a href="./pe_conspago.php">Consultar Informacion Pago Clientes</a></li>
         </ul>
         <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
             <input type="submit" value="Cerrar Sesion">
