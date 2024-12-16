fb4f080af6a30bb4a536b46e9bad8245e31a5e47
Sergio Quevedo <sergioquero@gmail.com>
2024-12-16 09:00:18 +0100

    BBDDPedidos

--- Ejercicios/BBDD/Web_Pedidos/pe_consped.php
+++ Ejercicios/BBDD/Web_Pedidos/pe_consped.php
@@ -7,6 +7,7 @@
         <h2>Alta de Pedidos</h2>
         <form action=<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?> method="post">
             <select name="clientes"><?php imprimirClientes() ?></select>
+            <input type="submit" value="Mostrar Informacion" name="mostrarInfo"><br>
         </form>
     <?php
         if($_SERVER["REQUEST_METHOD"] == "POST")
