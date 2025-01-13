0849d5ffda187434feb1e3a0ddfabd895754fe5a
Sergio Quevedo <sergioquero@gmail.com>
2025-01-13 10:17:06 +0100

    MovilMad

--- Ejercicios/BBDD/MovilMad/movlogin.php
+++ Ejercicios/BBDD/MovilMad/movlogin.php
@@ -1,11 +1,5 @@
 <html>
-   <?php include_once "funciones_session.php";
-        iniciarSession();
-        if(!verificarSessionExistente())
-        {
-            eliminarSession();
-        }
-   ?>
+   <?php include_once "funciones_session.php";?>
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
