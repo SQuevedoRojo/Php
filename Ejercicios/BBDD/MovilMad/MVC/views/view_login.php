7c67ecf135526224fc367e84361af5e9963dd9e1
Sergio Quevedo <sergioquero@gmail.com>
2025-01-14 13:37:42 +0100

    MVC

--- Ejercicios/BBDD/MovilMad/MVC/views/view_login.php
+++ Ejercicios/BBDD/MovilMad/MVC/views/view_login.php
@@ -4,9 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login Page - MovilMad</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./views/css/bootstrap.min.css">
  </head>
-      
+
 <body>
     <h1>MOVILMAD</h1>
 
