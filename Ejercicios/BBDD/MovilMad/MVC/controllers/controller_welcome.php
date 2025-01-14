73d4c98127ddf5d3a57d4e70ef52d14f48d38a6f
Sergio Quevedo <sergioquero@gmail.com>
2025-01-14 13:53:22 +0100

    MVC

---
+++ Ejercicios/BBDD/MovilMad/MVC/controllers/controller_welcome.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once ("controller_session.php");
+    require_once ("controller_comunes.php");    
+
+    require_once ("models/model_welcome.php");
+    require_once ("views/view_welcome.php");
+
+?>
---
+++ Ejercicios/BBDD/MovilMad/MVC/welcome.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once ("controllers/controller_welcome.php");
+
+?>
