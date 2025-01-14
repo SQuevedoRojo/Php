707c10e2864da44cc8db351c364e9f51edc60859
Sergio Quevedo <sergioquero@gmail.com>
2025-01-14 10:34:23 +0100

    MovilMad

--- Ejercicios/BBDD/MovilMad/funciones_movconsultar.php
+++ Ejercicios/BBDD/MovilMad/funciones_movconsultar.php
@@ -9,6 +9,7 @@
         if(isset($_POST["consultar"]))
         {
             list($fechaInicio,$fechaFinal) = recogerDatos();
+            imprimirVehiculosAlquiladosPeriodo($fechaInicio,$fechaFinal);
         }
     }
 
@@ -19,5 +20,42 @@
         var_dump($fechaInicio);
         var_dump($fechaFinal);
         return [$fechaInicio,$fechaFinal];
+    }
+
+    function saberVehiculosAlquiladosPeriodo($fechaInicio,$fechaFinal)
+    {
+        $conn = conexionBBDD();
+        try
+        {
+            $stmt = $conn->prepare("SELECT v.matricula,marca,modelo,fecha_devolucion,preciototal from rvehiculos v,ralquileres a where disponible = 'N' and idcliente=:idcliente and v.matricula = a.matricula and :fechaIni >= DATE(fecha_alquiler) and fecha_devolucion is not null and DATE(fecha_devolucion) <= :fechaFin");
+            $stmt->bindParam(':idcliente', $_SESSION["cliente"]["id"]);
+            $stmt->bindParam(':fechaIni', $fechaInicio);
+            $stmt->bindParam(':fechaFin', $fechaFinal);
+            $stmt -> execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $resultado=$stmt->fetchAll();
+        }
+        catch(PDOException $e)
+        {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $resultado;
+    }
+    
+    function imprimirVehiculosAlquiladosPeriodo($fechaInicio,$fechaFinal)
+    {
+        $resultado = saberVehiculosAlquiladosPeriodo($fechaInicio,$fechaFinal);
+        if($resultado == null)
+            print "<h2>No Ha Tenido Alquilado Ningun Vehiculo</h2>";
+        else
+        {
+            print "<table border='1'>";
+            print "<tr><th>Matricula</th><th>Marca</th><th>Modelo</th><th>Fecha Devolucion</th><th>Precio Total</th></tr>";
+            foreach ($resultado as $coche) {
+                print "<tr><th>".$coche["v.matricula"]."</th><th>".$coche["marca"]."</th><th>".$coche["modelo"]."</th><th>".$coche["fecha_devolucion"]."</th><th>".$coche["preciototal"]."€</th></tr>";
+            }
+            print "</table>";
+        }
     }
 ?>
