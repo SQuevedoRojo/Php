159f827ff0fb6ac5859203cc7a6e21991fa53adf
Sergio Quevedo <sergioquero@gmail.com>
2025-01-15 08:45:25 +0100

    MVC

--- Ejercicios/BBDD/MovilMad/MVC/controllers/controller_welcome.php
+++ Ejercicios/BBDD/MovilMad/MVC/controllers/controller_welcome.php
@@ -3,6 +3,13 @@
     require_once ("controller_session.php");
     require_once ("controller_comunes.php");    
     iniciarSession();
+    if(!verificarSessionExistente())
+    {
+        eliminarSession();
+        header("Location: index.php");
+    }
+    $id = devolverId();
+    $nombre = devolverNombre();
     require_once ("models/model_welcome.php");
     require_once ("views/view_welcome.php");
 
