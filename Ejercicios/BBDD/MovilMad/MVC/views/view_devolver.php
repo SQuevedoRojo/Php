4278f88457c5c1182514ff9ccbcc8d30d5e09bd6
Sergio Quevedo <sergioquero@gmail.com>
2025-01-20 09:55:32 +0100

    MVC

--- Ejercicios/BBDD/MovilMad/MVC/views/view_devolver.php
+++ Ejercicios/BBDD/MovilMad/MVC/views/view_devolver.php
@@ -46,11 +46,13 @@
             if(isset($params) && isset($signature) && $params != null && $signature != null)
             {
                 ?>
+                <div>
                 <form style="opacity: 0;" name="from" id="formularioPago" action="https://sis-t.redsys.es:25443/sis/realizarPago" method="POST">
                     <input type="hidden" name="Ds_SignatureVersion" value="HMAC_SHA256_V1"/>
                     <input type="hidden" name="Ds_MerchantParameters" value="<?php echo $params; ?>"/>
                     <input type="hidden" name="Ds_Signature" value="<?php echo $signature; ?>"/>	
                 </form>
+                </div>
                 <?php echo "<script>document.getElementById('formularioPago').submit();</script>";
             }
         ?>
