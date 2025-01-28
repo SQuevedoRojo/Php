46d8e759ac93541296ace6b01a1945ed552487fd
Sergio Quevedo <sergioquero@gmail.com>
2025-01-28 13:30:07 +0100

    MVC_Portal

--- Ejercicios/BBDD/Portal_Empleados/views/view_welcome.php
+++ Ejercicios/BBDD/Portal_Empleados/views/view_welcome.php
@@ -9,7 +9,7 @@
     <?php
     if($dept == "d003")
     {
-        print "<ol><a href='controller_altEmp.php'><li>Alta Empleado</li></a><a href='controller_altEmpMas.php'><li>Alta Masiva Empleados</li></a><a href='controller_modSal.php'><li>Modificar salario</li></a><li>Vida laboral</li><li>Info departamentos</li><li>Cambio departamento</li><li>Nuevo jefe departamento</li><li>Baja empleado</li><li>Mi nómina</li><li>Historial laboral</li></ol>";
+        print "<ol><a href='controller_altEmp.php'><li>Alta Empleado</li></a><a href='controller_altEmpMas.php'><li>Alta Masiva Empleados</li></a><a href='controller_modSal.php'><li>Modificar salario</li></a><a href='controller_vidLab.php'><li>Vida laboral</li></a><li>Info departamentos</li><li>Cambio departamento</li><li>Nuevo jefe departamento</li><li>Baja empleado</li><li>Mi nómina</li><li>Historial laboral</li></ol>";
     }
     else
     {
