d5940568f76b76aae4c4835bb3aa7e133782f61e
Sergio Quevedo <sergioquero@gmail.com>
2025-01-29 10:18:53 +0100

    MVC_Portal

--- Ejercicios/BBDD/Portal_Empleados/models/model_infDep.php
+++ Ejercicios/BBDD/Portal_Empleados/models/model_infDep.php
@@ -22,14 +22,17 @@
         try
         {
             $stmt = $GLOBALS["conn"]->prepare("SELECT dm.emp_no as emp_no, CONCAT(first_name, ' ',last_name) as nombre from dept_manager dm,employees e where dm.dept_no = :dept and dm.emp_no = e.emp_no");
+            $stmt->bindParam(':dept', $dept);
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $jefe=$stmt->fetchAll();
             $stmt = $GLOBALS["conn"]->prepare("SELECT de.emp_no as emp_no, CONCAT(first_name, ' ',last_name) as nombre from dept_emp de,employees e where de.dept_no = :dept and de.emp_no = e.emp_no and to_date is null");
+            $stmt->bindParam(':dept', $dept);
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $empleadosTrabajando=$stmt->fetchAll();
             $stmt = $GLOBALS["conn"]->prepare("SELECT de.emp_no as emp_no, CONCAT(first_name, ' ',last_name) as nombre from dept_emp de,employees e where de.dept_no = :dept and de.emp_no = e.emp_no and to_date is not null");
+            $stmt->bindParam(':dept', $dept);
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $empleadosCambiados=$stmt->fetchAll();
