071826b0984e615a0c3fa0f3d92392c47b191524
Sergio Quevedo <sergioquero@gmail.com>
2025-01-30 08:45:35 +0100

    MVC_Portal

--- Ejercicios/BBDD/Portal_Empleados/views/view_welcome.php
+++ Ejercicios/BBDD/Portal_Empleados/views/view_welcome.php
@@ -9,7 +9,7 @@
     <?php
     if($dept == "d003")
     {
-        print "<ol><a href='controller_altEmp.php'><li>Alta Empleado</li></a><a href='controller_altEmpMas.php'><li>Alta Masiva Empleados</li></a><a href='controller_modSal.php'><li>Modificar salario</li></a><a href='controller_vidLab.php'><li>Vida laboral</li></a><a href='controller_infDep.php'><li>Info departamentos</li></a><a href='controller_camDept.php'><li>Cambio departamento</li></a><a href='controller_camJefDept.php'><li>Nuevo Jefe Departamento</li></a><li>Baja empleado</li><li>Mi nómina</li><li>Historial laboral</li></ol>";
+        print "<ol><a href='controller_altEmp.php'><li>Alta Empleado</li></a><a href='controller_altEmpMas.php'><li>Alta Masiva Empleados</li></a><a href='controller_modSal.php'><li>Modificar salario</li></a><a href='controller_vidLab.php'><li>Vida laboral</li></a><a href='controller_infDep.php'><li>Info departamentos</li></a><a href='controller_camDept.php'><li>Cambio departamento</li></a><a href='controller_camJefDept.php'><li>Nuevo Jefe Departamento</li></a><a href='controller_bajaEmp.php'><li>Baja empleado</li></a><li>Mi nómina</li><li>Historial laboral</li></ol>";
     }
     else
     {
