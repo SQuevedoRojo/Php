ae199b45bac26a8ebbacc532400be4d90a3d2f3c
Sergio Quevedo <sergioquero@gmail.com>
2025-02-13 14:01:05 +0100

    appmusic

--- appmusic/models/model_downmusic.php
+++ appmusic/models/model_downmusic.php
@@ -4,8 +4,8 @@
     {
         try
         {
-            $stmt = $GLOBALS["conn"]->prepare("SELECT t.TrackId as TrackId,t.Name as TName,a.Title as ATitle,ar.Name as ArName,m.Name as MName,g.Name as GName,t.Composer as TComposer,t.UnitPrice as TUnitPrice from track t,album a,artist ar,mediatype m,genre g where t.AlbumId = a.AlbumId and a.ArtistId = ar.ArtistId and t.MediaTypeId = m.MediaTypeId and t.GenreId = g.GenreId order by t.TrackId limit 10 offset :indice");
-            $stmt->bindParam(':indice', $indice);
+            $stmt = $GLOBALS["conn"]->prepare("SELECT t.TrackId as TrackId,t.Name as TName,a.Title as ATitle,ar.Name as ArName,m.Name as MName,g.Name as GName,t.Composer as TComposer,t.UnitPrice as TUnitPrice from track t,album a,artist ar,mediatype m,genre g where t.AlbumId = a.AlbumId and a.ArtistId = ar.ArtistId and t.MediaTypeId = m.MediaTypeId and t.GenreId = g.GenreId order by t.TrackId limit 10 offset $indice");
+            //$stmt->bindParam(':indice', $indice);
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $resultado=$stmt->fetchAll();
