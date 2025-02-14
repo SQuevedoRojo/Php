750f5726e8eea1d0482f7f10fa84b7e3dcf0deed
Sergio Quevedo <sergioquero@gmail.com>
2025-02-14 09:05:07 +0100

    appmusic

--- appmusic/views/view_downmusic.php
+++ appmusic/views/view_downmusic.php
@@ -4,13 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Descargar Musica</title>
-    <style>
-        #cesta
-        {
-            position: relative;
-            right: 90%;
-        }
-    </style>
  </head>
 
 <body>
