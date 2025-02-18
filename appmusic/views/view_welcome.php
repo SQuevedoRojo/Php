4d41935078ac740449373efb9d55d88735112719
Sergio Quevedo <sergioquero@gmail.com>
2025-02-18 11:58:50 +0100

    appmusic

--- appmusic/views/view_welcome.php
+++ appmusic/views/view_welcome.php
@@ -9,7 +9,7 @@
     <ol>
         <li><a href="controller_downmusic.php">DownMusic</a></li>
         <li><a href="controller_histfacturas.php">Historial de Facturas</a></li>
-        <li><a href="">Factura Determinada</a></li>
+        <li><a href="controller_facturas.php">Factura Determinada</a></li>
         <li><a href="">Ranking</a></li>
     </ol>
     <a href="controller_cerrarSession.php">Cerrar Sesion</a>
