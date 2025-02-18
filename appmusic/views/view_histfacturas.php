84f6fd79735d876916eff7db6c5af9e88340bbb8
Sergio Quevedo <sergioquero@gmail.com>
2025-02-18 10:51:16 +0100

    appmusic

--- appmusic/views/view_histfacturas.php
+++ appmusic/views/view_histfacturas.php
@@ -10,12 +10,25 @@
     <?php
         if(isset($datosFacturas))
         {
-            print "<table border='1'><tr><th>Customer ID</th><th>Invoice ID</th><th>Invoice Line ID</th><th>Invoice Date</th><th>Track ID</th><th>Unit Price</th><th>Quantity</th><th>Total</th></tr>";
+            $invoiceID = $datosFacturas[0]["InvoiceId"];
+            $mostrarCabecera = true; 
             foreach ($datosFacturas as $datos)
             {
-                print "<tr><th>".$datos["CustomerId"]."</th><td>".$datos["InvoiceId"]."</td><td>".$datos["InvoiceLineId"]."</td><td>".$datos["InvoiceDate"]."</td><td>".$datos["TrackId"]."</td><td>".$datos["UnitPrice"]."</td><th>".$datos["Quantity"]."</td><td>".$datos["Total"]."</td></tr>";
+                if($mostrarCabecera)
+                {
+                    print "<h2><strong>Cliente : ".$datos["CustomerId"]." | Invoice ID : $invoiceID</strong></h2>";
+                    print "\t<table border='1'><tr><th>Invoice Line ID</th><th>Invoice Date</th><th>Track ID</th><th>Unit Price</th><th>Quantity</th><th>Total</th></tr>";
+                    $mostrarCabecera = false;
+                }
+                print "\t<tr><td>".$datos["InvoiceLineId"]."</td><td>".$datos["InvoiceDate"]."</td><td>".$datos["TrackId"]."</td><td>".$datos["UnitPrice"]."</td><th>".$datos["Quantity"]."</td><td>".$datos["Total"]."</td></tr>";
+                $invId = $datos["InvoiceId"];
+                if($invId != $invoiceID)
+                {
+                    print "\t</table><br>";
+                    $mostrarCabecera = true;;
+                }
             }
-            print "</table>";
+            
         }
     ?>
 
