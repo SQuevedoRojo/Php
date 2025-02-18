b59c64d036284ff4d3d10224fed9f01ecef5f5e6
Sergio Quevedo <sergioquero@gmail.com>
2025-02-18 12:00:36 +0100

    appmusic

--- appmusic/models/model_facturas.php
+++ appmusic/models/model_facturas.php
@@ -1,10 +1,12 @@
 <?php
 
-    function recuperarDatosCompra($idCliente)
+    function recuperarDatosCompra($idCliente,$fecIni,$fecFin)
     {
         try
         {
             $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente and DATE(InvoiceDate) >= :fecIni and DATE(InvoiceDate) <= :fecFin order by 1,2");
+            $stmt->bindParam(':fecIni', $fecIni);
+            $stmt->bindParam(':fecFin', $fecFin);
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $resultado=$stmt->fetchAll();
