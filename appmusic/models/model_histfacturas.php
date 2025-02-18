cc65bdbcea45037fcfe59cc787e760967f855ff0
Sergio Quevedo <sergioquero@gmail.com>
2025-02-18 11:00:04 +0100

    appmusic

--- appmusic/models/model_histfacturas.php
+++ appmusic/models/model_histfacturas.php
@@ -4,7 +4,7 @@
     {
         try
         {
-            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente order by 1,2 LIMIT 6723");
+            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente order by 1,2 LIMIT 4482");
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $resultado=$stmt->fetchAll();
