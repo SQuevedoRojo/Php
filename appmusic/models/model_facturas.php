ad1c356cc4ebb63c4585e0957cb159641c214b99
Sergio Quevedo <sergioquero@gmail.com>
2025-02-20 13:23:16 +0100

    appmusic

--- appmusic/models/model_facturas.php
+++ appmusic/models/model_facturas.php
@@ -4,7 +4,7 @@
     {
         try
         {
-            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente and DATE(InvoiceDate) >= :fecIni and DATE(InvoiceDate) <= :fecFin order by 1,2");
+            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente and DATE(InvoiceDate) >= :fecIni and DATE(InvoiceDate) <= :fecFin and i.InvoiceId = il.InvoiceId order by 1,2");
             $stmt->bindParam(':fecIni', $fecIni);
             $stmt->bindParam(':fecFin', $fecFin);
             $stmt -> execute();
--- appmusic/models/model_histfacturas.php
+++ appmusic/models/model_histfacturas.php
@@ -4,7 +4,7 @@
     {
         try
         {
-            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente order by 1,2 LIMIT 4482");
+            $stmt = $GLOBALS["conn"]->prepare("SELECT i.InvoiceId as InvoiceId,InvoiceLineId,CustomerId,InvoiceDate,Total,TrackId,UnitPrice,Quantity from invoice i,invoiceline il where CustomerId = $idCliente and i.InvoiceId = il.InvoiceId order by 1,2 LIMIT 4482");
             $stmt -> execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $resultado=$stmt->fetchAll();
